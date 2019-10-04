077715c36d24b3925a83703bb1619c026db9494e
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2019-10-04 19:11:02 +0300

    remove unnecessary changes

--- packages/core/src/filters/FilterSystem.js
+++ packages/core/src/filters/FilterSystem.js
@@ -100,7 +100,7 @@ export class FilterSystem extends System
          * @member {Object[]}
          * @readonly
          */
-        this.defaultFilterStack = [new FilterState()];
+        this.defaultFilterStack = [{}];
 
         /**
          * stores a bunch of PO2 textures used for filtering
